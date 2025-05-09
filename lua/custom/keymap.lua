8509f676b25f5ba92d5b1a2e20003bc94dd505cf
Quatton <atomicatives@gmail.com>
2025-05-09 13:19:31 +0900

    added remote sshfs (but useless

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -4,4 +4,4 @@ keymap.set('n', '<D-z>', 'u', { noremap = true, silent = true })
 keymap.set('n', '<D-Z>', '<C-r>', { noremap = true, silent = true })
 keymap.set('n', '<C-a>', 'ggVG', { noremap = true, silent = true })
 
-keymap.set('n', '<leader>cc', ':CopilotChat<CR>', { noremap = true, silent = true })
+keymap.set('n', '<leader>ca', ':CopilotChat<CR>', { noremap = true, silent = true, desc = 'Copilot Chat' })
